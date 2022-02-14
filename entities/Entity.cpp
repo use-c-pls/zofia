b29227898bf70bf9016cc20e2a88fb84becb2c2c
kien2111 <nhox_kute_Kien2111@yahoo.com>
2022-02-14 18:46:58 +0700

    add entity

--- entities/Entity.cpp
+++ entities/Entity.cpp
@@ -2,4 +2,38 @@
 // Created by kienle on 13/02/2022.
 //
 
-#include "Entity.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#define ZOFIA zofia::
+namespace zofia {
+    class Entity {
+        private:
+            //Entity properties
+            sf::Texture m_texture;
+            sf::Sprite m_sprite;
+        public:
+            explicit Entity(){
+                this->init();
+            }
+            ~Entity();
+            virtual void init();
+            virtual void updateState();
+            virtual void draw(sf::RenderWindow& window);
+    };
+}
+
+/*
+void ZOFIA Entity::init(){
+
+}
+
+void ZOFIA Entity::updateState() {
+
+}
+
+void ZOFIA Entity::draw(sf::RenderWindow& window){
+}*/
